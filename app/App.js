f67a2fa75667a5b9d843e5cf2438404ad09c3e1d
wnsgudchl0302 <wnsgudchl0302@naver.com>
2021-04-16 15:26:34 +0900

    0416수업 내용

--- app/App.js
+++ app/App.js
@@ -6,14 +6,12 @@
  * @flow strict-local
  */
 
- import React, { Component } from 'react';
- import type { Node } from 'react';
+ import React, {Component} from 'react';
+ import type {Node} from 'react';
  import Heading from './Heading'
  import Input from './Input'
  import Button from './Button'
  import TodoList from './TodoList'
- 
- let todoIndex = 0
  
  import {
    SafeAreaView,
@@ -33,64 +31,87 @@
    ReloadInstructions,
  } from 'react-native/Libraries/NewAppScreen';
  
+ let todoIndex = 0;
  class App extends Component {
-   constructor() {
-     super()
-     this.state = {
-       inputValue: '',
-       todos: [],
-       type: 'All'
-     }
+  constructor() {
+   super()
+   this.state = {
+     inputValue: '',
+     todos: [],
+     type: 'All'
    }
-   inputChange(inputValue) {
-     console.log(' Input Value: ', inputValue)
-     this.setState({ inputValue })
-   }
+   this.submitTodo = this.submitTodo.bind(this);
+   this.toggleComplete = this.toggleComplete.bind(this);
+   this.deleteTodo = this.deleteTodo.bind(this);
+  }
+  inputChange(inputValue) {    
+     console.log(' Input Value: ' , inputValue);    
+     this.setState({ inputValue })    
+  }
  
-   submitTodo = () => {    
-     if (this.state.inputValue.match(/^\s*$/)) {    
-       return    
-     }    
-     const todo = {    
-       title: this.state.inputValue,    
-       todoIndex,    
-       complete: false    
-     }    
-     todoIndex++    
-     const todos = [...this.state.todos, todo]    
-     this.setState({ todos, inputValue: '' }, () => {    
-       console.log('State: ', this.state)    
-     }) 
-   }
+  submitTodo = () => {    
+    if (this.state.inputValue.match(/^\s*$/)) {    
+      return    
+    }    
+    const todo = {    
+      title: this.state.inputValue,    
+      todoIndex,    
+      complete: false    
+    }    
+    todoIndex++
+    const todos = [...this.state.todos, todo]    
+    this.setState({ todos, inputValue: '' }, () => {    
+      console.log('State: ', this.state)    
+    }) 
+  }
  
-   render () {
-     const { inputValue, todos } = this.state
-     return (
-       <View style={styles.container}>
-         <ScrollView
-           keyboardShouldPersistTaps='always'
-           style={styles.content}>
-           <Heading />
-           <Input inputValue={inputValue} inputChange={(text) => this.inputChange(text)} />
-           <TodoList todos={todos} />
-           <Button submitTodo={this.submitTodo} />
-         </ScrollView>
-       </View>
-     )
-   }
+  deleteTodo = (todoIndex) => {
+    let {todos} = this.state
+    todos = todos.filter((todo) => todo.todoIndex !== todoIndex)
+    this.setState({todos})
+  }
  
+  toggleComplete = (todoIndex) => {
+    let todos = this.state.todos
+    todos.forEach((todo) => {
+      if(todo.todoIndex === todoIndex) {
+        todo.complete = !todo.complete
+      }
+    })
+    this.setState({todos})
+  }
+ 
+  render() {
+    const { inputValue, todos } = this.state
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          keyboardShouldPersistTaps='always'
+          style={styles.content}>
+          <Heading />
+          <Input
+            inputValue={inputValue}
+            inputChange={(text) => this.inputChange(text)} />
+            <TodoList
+             toggleComplete={this.toggleComplete}
+             deleteTodo={this.deleteTodo}
+             todos={todos} />
+            <Button submitTodo={this.submitTodo} />
+        </ScrollView>
+      </View>
+    )
+  }
  }
  
- 
  const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     backgroundColor: '#f5f5f5'
-   },
-   content: {
-     flex: 1,
-     paddingTop: 60
-   }
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5'
+  },
+  content: {
+    flex: 1,
+    paddingTop: 60
+  }
  })
  
  export default App;
--- app/TodoList.js
+++ app/TodoList.js
@@ -2,17 +2,19 @@ import React from 'react'
 import { View } from 'react-native'
 import Todo from './Todo'
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, deleteTodo, toggleComplete }) => {
   todos = todos.map((todo, i) => {
     return (
-      <Todo
-        key={todo.todoIndex}
-        todo={todo} />
+        <Todo
+            deleteTodo={deleteTodo}
+            toggleComplete={toggleComplete}
+            key={i}
+            todo={todo} />
     )
   })
   return (
     <View>
-      {todos}
+        {todos}
     </View>
   )
 }
